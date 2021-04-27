0a0cc3161db15f5b59e03e7d6e9ac32623d795c0
lbalbrecht <lalbrecht18@wooster.edu>
2021-04-26 22:51:31 -0700

    removed separate crud js files for the sake of simplicity

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 
 // CRUD elements organized into separate js files for the sake of making code readable
-const add = require('./crud-elements/add');
-const view = require('./crud-elements/view');
-const update = require('./crud-elements/update');
-const del = require('./crud-elements/delete')
+// const add = require('./crud-elements/add');
+// const view = require('./crud-elements/view');
+// const update = require('./crud-elements/update');
+// const del = require('./crud-elements/delete')
 
 // establish mysql server
 const connection = mysql.createConnection({
@@ -85,15 +85,15 @@ const addData = () => {
         .then((answer) => {
             switch (answer.addChoice) {
                 case 'Add new department':
-                    add.addDepartment();
+                    addDepartment();
                     break;
 
                 case 'Add new role':
-                    add.addRole();
+                    addRole();
                     break;
 
                 case 'Add new employee':
-                    add.addEmployee();
+                    addEmployee();
                     break;
 
                 default:
@@ -103,123 +103,225 @@ const addData = () => {
         })
 };
 
-// const addDepartment = () => {
-//     inquirer
-//         .prompt({
-//             name: 'newDepartment',
-//             type: 'input',
-//             message: 'Please enter the title of your new department',
-//         })
-//         .then((answer) => {
-//             console.log('Inserting a new product...\n');
-//             const query = connection.query(
-//                 'INSERT INTO department SET ?',
-//                 {
-//                     name: answer.newDepartment
-//                 },
-//                 (err, res) => {
-//                     if (err) throw err;
-//                     console.log(`${res.affectedRows} department added!\n`);
-//                     // Call updateProduct AFTER the INSERT completes
-//                     // updateProduct();
-//                 }
-//             );
+// adds a new department
+const addDepartment = () => {
+    inquirer
+        .prompt({
+            name: 'newDepartment',
+            type: 'input',
+            message: 'Please enter the title of your new department',
+        })
+        .then((answer) => {
+            console.log('Adding a new department...\n');
+            const query = connection.query(
+                'INSERT INTO department SET ?',
+                {
+                    name: answer.newDepartment
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`${res.affectedRows} department added!\n`);
+                }
+            );
+            console.log(query.sql);
+        });
+};
 
-//             // logs the actual query being run
-//             console.log(query.sql);
-//         });
-// };
+// adds a new role
+const addRole = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'newRole',
+                type: 'input',
+                message: 'Please enter the title of your new role',
+            },
+            {
+                name: 'roleSalary',
+                type: 'input',
+                message: 'Please enter the salary for this role',
+            },
+            {
+                name: 'roleDepartment',
+                type: 'input',
+                message: 'Please enter the department this role is in',
+            },
+        ])
+        .then((answer) => {
+            console.log('Adding a new role...\n');
+            const query = connection.query(
+                'INSERT INTO role SET ?',
+                {
+                    name: answer.newRole,
+                    salary: answer.newSalary
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`${res.affectedRows} role added!\n`);
+                }
+            );
 
-// // inquirer prompt for the user to view specific information in the database if they select 'view'
-// const viewData = () => {
-//     inquirer.prompt({
-//         type: 'rawlist',
-//         name: 'viewChoice',
-//         message: 'What would you like to view?',
-//         choices: [
-//             'View department',
-//             'View role',
-//             'View employee',
-//         ]
-//     })
-//     .then((answer) => {
-//         switch (answer.userChoice) {
-//             case 'View department':
-//                 view.viewDepartment();
-//                 break;
+            console.log(query.sql);
+        });
+};
 
-//             case 'View role':
-//                 view.viewRole();
-//                 break;
+// adds a new employee
+const addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'firstName',
+                type: 'input',
+                message: "Please enter the employee's first name",
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: "Please enter the employee's last name",
+            },
+            {
+                name: 'employeeRole',
+                type: 'input',
+                message: "Please enter the employee's role",
+            },
+            {
+                name: 'employeeManager',
+                type: 'input',
+                message: "Please enter the employee's manager",
+            },
+        ])
+        .then((answer) => {
+            console.log('Adding a new employee...\n');
+            const query = connection.query(
+                'INSERT INTO role SET ?',
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`${res.affectedRows} role added!\n`);
+                }
+            );
 
-//             case 'View employee':
-//                 view.viewEmployee();
-//                 break;
+            console.log(query.sql);
+        });
+}
 
-//             default:
-//                 console.log(`Invalid action: ${answer.action}`);
-//                 break;
-//         }
-//     })
-// };
+// inquirer prompt for the user to view specific information in the database if they select 'view'
+const viewData = () => {
+    inquirer.prompt({
+        type: 'rawlist',
+        name: 'viewChoice',
+        message: 'What would you like to view?',
+        choices: [
+            'View departments',
+            'View roles',
+            'View employees',
+        ]
+    })
+    .then((answer) => {
+        switch (answer.userChoice) {
+            case 'View department':
+                viewDepartment();
+                break;
 
-// // inquirer prompt for the user to update information within the database if they select 'update'
-// const updateData = () => {
-//     inquirer.prompt({
-//         type: 'rawlist',
-//         name: 'viewChoice',
-//         message: 'What would you like to view?',
-//         choices: [
-//             'Update employee role',
-//             'Update employee manager'
-//         ]
-//     })
-//     .then((answer) => {
-//         switch (answer.userChoice) {
-//             case 'Update employee role':
-//                 update.updateRole();
-//                 break;
+            case 'View role':
+                viewRole();
+                break;
 
-//             case 'Update employee manager':
-//                 update.updateManager();
-//                 break;
+            case 'View employee':
+                viewEmployee();
+                break;
 
-//             default:
-//                 console.log(`Invalid action: ${answer.action}`);
-//                 break;
-//         }
-//     })
-// };
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                break;
+        }
+    })
+};
 
-// // inquirer prompt for the user to delete information from the database if they select 'delete'
-// const deleteData = () => {
-//     inquirer.prompt({
-//         type: 'rawlist',
-//         name: 'viewChoice',
-//         message: 'What would you like to delete?',
-//         choices: [
-//             'Delete department',
-//             'Delete role',
-//             'Delete employee',
-//         ]
-//     })
-//     .then((answer) => {
-//         switch (answer.userChoice) {
-//             case 'Delete department':
-//                 del.deleteDepartment();
-//                 break;
+// view registered departments
+const viewDepartment = () => {
+    connection.query('SELECT name FROM department', (err, res) => {
+        if (err) throw err;
+        console.log(res);
+    });
+}
 
-//             case 'Delete role':
-//                 del.deleteRole();
-//                 break;
+const viewRole = () => {
+    connection.query('SELECT name FROM role', (err, res) => {
+        if (err) throw err;
+        console.log(res);
+    });
+}
 
-//             case 'Delete employee':
-//                 del.deleteEmployee();
-//                 break;
+const viewEmployee = () => {
+    connection.query('SELECT first_name, last_name FROM employee', (err, res) => {
+        if (err) throw err;
+        console.log(res);
+    });
+}
 
-//             default:
-//                 console.log(`Invalid action: ${answer.action}`);
-//                 break;
-//         }
-//     })
-// }
+
+
+// inquirer prompt for the user to update information within the database if they select 'update'
+const updateData = () => {
+    inquirer.prompt({
+        type: 'rawlist',
+        name: 'viewChoice',
+        message: 'What would you like to view?',
+        choices: [
+            'Update employee role',
+            'Update employee manager'
+        ]
+    })
+    .then((answer) => {
+        switch (answer.userChoice) {
+            case 'Update employee role':
+                update.updateRole();
+                break;
+
+            case 'Update employee manager':
+                update.updateManager();
+                break;
+
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                break;
+        }
+    })
+};
+
+// inquirer prompt for the user to delete information from the database if they select 'delete'
+const deleteData = () => {
+    inquirer.prompt({
+        type: 'rawlist',
+        name: 'viewChoice',
+        message: 'What would you like to delete?',
+        choices: [
+            'Delete department',
+            'Delete role',
+            'Delete employee',
+        ]
+    })
+    .then((answer) => {
+        switch (answer.userChoice) {
+            case 'Delete department':
+                del.deleteDepartment();
+                break;
+
+            case 'Delete role':
+                del.deleteRole();
+                break;
+
+            case 'Delete employee':
+                del.deleteEmployee();
+                break;
+
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                break;
+        }
+    })
+}
--- schema.sql
+++ schema.sql
@@ -5,20 +5,20 @@ CREATE DATABASE employee_trackerdb;
 USE employee_trackerdb;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INTEGER(10) NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INTEGER(10) NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INTEGER(10) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
